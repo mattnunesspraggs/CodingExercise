43fb295921288e8354d9d635f19bc8ebb0045eed
Matt Nunes-Spraggs <163141+mattnunes@users.noreply.github.com>
2020-05-08 14:32:30 -0700

    Initial commit of the iOS target.
    	- Added an xcodeproj and a universal iOS target.
    
    Signed-off-by: Matt Nunes-Spraggs <163141+mattnunes@users.noreply.github.com>

---
+++ Directory-iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+// ViewController.swift
+// Copyright © 2020 Matt Nunes-Spraggs
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
